2d751e965a475e4fc586a38e92b203c24aa145ef
Ichan Kabir <ichankabir@gmail.com>
2022-04-08 00:20:52 +0530

    feature: parallax effect added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const  App = () => {
+// Project Imports
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const App = () => {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      {/* Navbar Here */}
+      <Navbar />
+      <div style={{ marginTop: "48px" }} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
