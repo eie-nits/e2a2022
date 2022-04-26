fbc2526f2e3017bd1d17960a2034e0f1b73dd8b5
Ichan Kabir <ichankabir@gmail.com>
2022-04-27 00:21:25 +0530

    call for paper accordion

---
+++ src/assets/papers.js
@@ -0,0 +1,76 @@
+export default [
+  {
+    heading: "Control And Robotics",
+    items: [
+      "Linear Control", 
+      "Non-linear Control", 
+      "Robust Control", 
+      "Decentralized Control", 
+      "Robotics", 
+      "Industrial Automation", 
+      "Stability Analysis", 
+      "Fractional Order Control"]
+  },
+  {
+    heading: "Image and Video Signal Processing",
+    items: [
+      "Image and Video Signal Processing",
+      "Multimedia Applications",
+      "Signal Processing for Big Data",
+      "Speech and Audio Processing",
+      "Computer Vision",
+      "Biomedical Applications, Bio-Signal Analysis",
+      "Language Identification",
+      "Information Forensics & Security",
+      "Sparse Signal Processing",
+      "Time-Frequency Analysis",
+      "Active Noise Control",
+      "Adapative Signal Processing",
+      "Array Signal Processing",
+    ]
+  },
+  {
+    heading: "Sensors and Instrumentation",
+    items: [
+      "Methods of Measurements",
+      "Electromagnetics, Digital Holography, Laser based Instrumentation for Measurement and Monitoring systems ", 
+      "Sensors and Sensing Technology",
+      "Non-Destructive Testing", 
+      "Smart and Intelligent Sensors",
+      "Biomedical Sensors, Signal Analysis & Health Monitoring Applications ",
+      "IoT Applications in Instrumentation",
+      "Sensors& Sensor Data Fusion",
+      "Robotics & Mechatronics Systems",
+      "Process Control & Instrumentation.",
+    ]
+  },
+  {
+    heading: "MEMS, Energy Harvesters",
+    items: [
+      "MEMS, Energy Harvesters",
+      "Nanotechnology, Renewable Energy, Nano Lithography Processes ",
+      "Solar Cells, Photonics",
+      "FinFet Technology",
+      "Memory Devices",
+      "VLSI Device Modelling",
+      "Advanced Materials, Characterization and Applications.",
+    ]
+  },
+  {
+    heading: "AI &amp; Soft Computing",
+    items: [
+      "AI and Machine Learning",
+      "CAD System",
+      "Deep Learning Applications ",
+      "AI in Healthcare",
+      "Pattern Recognition",
+      "Biometrics",
+      "Evolutionary Computing",
+      "Hybrid Intelligent Systems",
+      "E-commerce, E-medicine",
+      "Morphic Computing",
+      "Pervasive computing and Ambient Intelligence",
+      "Data Visualization."
+    ]
+  }
+]
